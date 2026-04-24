f1c017cdff609f9d8af7a1d027fc423916a0af8a
GauravMittal2712 <gm3814@srmist.edu.in>
2026-04-24 12:19:05 +0530

    Pushed

---
+++ Tasks/Day10/Day10Q1.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+public class Day10Q1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        calculate_wages1 cal =  new calculate_wages1();
+        cal.calculate();
+    }
+
+}
+class employeedata1 {
+    public final int employee_wageperhour = 20;
+    public final int Employee_fulltime = 8;
+    public final int Employee_parttimehour = 4;
+    public final int maxi_days = 20;
+    public final int maxi_hours = 100;
+}
+
+class calculate_wages1 extends employeedata{
+    int total_emp_hour = 0;
+    int total_emp_days=0;
+    Random random = new Random();
+    int emphour=0;
+
+    void calculate(){
+        while(total_emp_hour < maxi_hours && total_emp_days < maxi_days){
+            total_emp_days++;
+            int empcheck = random.nextInt(3);
+            switch (empcheck){
+                case 1:
+                    emphour = Employee_fulltime;
+                    System.out.println("Day "+ emphour+" Full Time");
+                    break;
+                case 2:
+                    emphour = Employee_parttimehour;
+                    System.out.println("Day "+emphour+" Part Time");
+                    break;
+                default:
+                    emphour=0;
+                    System.out.println("Day "+emphour+" Absent");
+                    break;
+
+            }
+            total_emp_hour+=emphour;
+            int daily_wages = total_emp_hour*employee_wageperhour;
+            System.out.println("Hours Worked " + emphour);
+            System.out.println("Daily Wages "+daily_wages);
+        }
+        int salary = total_emp_hour*employee_wageperhour;
+        System.out.println("Total Working Days: " + total_emp_days);
+        System.out.println("Total Working Hours: " + total_emp_hour);
+        System.out.println("Total Salary: " + salary);
+    }
+}
+
+
+
